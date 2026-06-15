38283c029501247a757def2753216bfdfadfb3e5
alanis07 <151046540+alanis07@users.noreply.github.com>
2026-06-14 21:02:45 -0300

    Add files via upload

--- ContaBanco.js
+++ ContaBanco.js
@@ -1,20 +1,49 @@
-class ContaBancaria {
-    #saldoAtual;
-    #senhaCliente;
+public class ContaBanco {
 
-    constructor() {
-        this.#saldoAtual = 400;
-        this.#senhaCliente = "072009";
-    }
-
-    versaldo(senha) {
-        if (senha === this.#senhaCliente) {
-            return `Saldo Atual: R$ ${this.#saldoAtual}`;
-        }
-        return "Senha incorreta";
-    }
+    private double saldoAtual = 400.00;
+    private String nomeCliente = "Alanis";
+    private String senhaCliente = "072009";
+    private String agenciaCliente = "MercadoPago";
+    private int numeroConta = 430;
 }
 
-const conta = new ContaBancaria();
+public void sacaValor(double valor) {
+    if (valor <= saldoAtual) {
+            saldoAtual -= valor;
+            System.out.println("Saque realizado com sucesso!");
+    }
+    else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
 
-console.log(conta.versaldo("072009"));
+public void depositarValor(double valor) {
+        saldoAtual += valor;
+        System.out.println("Depósito realizado com sucesso!");
+    }
+
+public boolean validarSenha(String senha) {
+        return senhaCliente.equals(senha);
+    }
+
+public void verSaldo() {
+        System.out.println("Saldo atual: R$ " + saldoAtual);
+    }
+
+public static void main(String[] args) {
+
+        ContaBanco conta = new ContaBanco();
+
+        if (conta.validarSenha("072009")) {
+            System.out.println("Acesso permitido!");
+            conta.verSaldo();
+
+            conta.depositarValor(100);
+            conta.verSaldo();
+
+            conta.sacaValor(50);
+            conta.verSaldo();
+        } else {
+            System.out.println("Senha incorreta!");
+        }
+    }
